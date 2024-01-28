535e4ee7dd5907ca5e7c14ab415654865f0f6b80
Peppisparrow <127871840+Peppisparrow@users.noreply.github.com>
2024-01-28 20:12:33 +0100

    creation form createTournament

--- IT/codekatabattle-frontend/src/components/CreateTournament.tsx
+++ IT/codekatabattle-frontend/src/components/CreateTournament.tsx
@@ -1,6 +1,63 @@
-export const CreateTournament= () => {
+import {useForm} from "react-hook-form";
+import {SubmitHandler} from "react-hook-form";
 
-    return(
+enum Policy{
+    friendOnly="friendsOnly",
+    public="public",
+    private="private"
+}
+interface ICreateTournamentForm{
+    tournamentTitle: string
+    tournamentDescription: string
+    ETD: string
+    FTD: string
+    maxNumberOfStudents: number
+    tournamentPolicy: Policy
+}
+export default function CreateTournament() {
+    const {
+        register,
+        handleSubmit,
+    } = useForm<ICreateTournamentForm>()
+    const onSubmit: SubmitHandler<ICreateTournamentForm> = (data) => console.log(data)
+
+
+    return (
+        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="text-3xl font-bold" style={{padding: "1%"}}>Create Tournament</h1>
+            {/* register your input into the hook by invoking the "register" function */}
+            <div style={{padding: "1%"}}>
+                <input className="textarea textarea-primary" {...register("tournamentTitle", {required: true})}
+                       placeholder="Tournament Title..." style={{width: "33.33%"}}/>
+            </div>
+            <div style={{padding: "1%"}}>
+                <input className="textarea textarea-primary" {...register("tournamentDescription")}
+                       style={{width: "99%"}} placeholder="Description..."/>
+            </div>
+            <div style={{padding: "1%"}}>
+                <input type="date" {...register("ETD", {required: true})} />
+                <input type="date" {...register("FTD", {required: true})} />
+            </div>
+            <div style={{padding: "1%"}}>
+                <input  className="textarea textarea-primary"{...register("maxNumberOfStudents", {required: true})} placeholder="Max students allowed..."/>
+            </div>
+            <div style={{padding: "1%"}}>
+                <select {...register("tournamentPolicy")}>
+                    <option value="friendsOnly">Friends Only</option>
+                    <option value="public">Public</option>
+                    <option value="private">Private</option>
+                </select>
+
+            </div>
+            <div style={{padding: "1%"}}>
+                <input type="submit"/>
+            </div>
+        </form>
+)
+
+/*return (
         <>
             <h1 className="text-3xl font-bold" style={{padding:"1%"}}>Create Tournament</h1>
 
@@ -21,5 +78,5 @@ export const CreateTournament= () => {
             </form>
         </>
 
-    )
+    )*/
 }
--- IT/codekatabattle-frontend/src/components/PageCore.tsx
+++ IT/codekatabattle-frontend/src/components/PageCore.tsx
@@ -1,7 +1,7 @@
 import {GithubLoginButton} from "./GithubLoginButton.tsx";
 import {useContext} from "react";
 import {AuthContext} from "../context/AuthContext.ts";
-import {CreateTournament} from "./CreateTournament.tsx";
+import CreateTournament from "./CreateTournament.tsx";
 
 export const PageCore= () => {
     const {user}=useContext(AuthContext);
