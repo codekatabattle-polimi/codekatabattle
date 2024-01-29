732a79086d4e524176dca10dda45b804e92349d3
Nick <nicolo.giallongo@mail.polimi.it>
2024-01-29 11:13:01 +0100

    Create tournament page updated

--- IT/codekatabattle-frontend/src/components/CreateTournament.tsx
+++ IT/codekatabattle-frontend/src/components/CreateTournament.tsx
@@ -36,12 +36,31 @@ export default function CreateTournament() {
                 <input className="textarea textarea-primary" {...register("tournamentDescription")}
                        style={{width: "99%"}} placeholder="Description..."/>
             </div>
-            <div style={{padding: "1%"}}>
-                <input type="date" {...register("ETD", {required: true})} />
-                <input type="date" {...register("FTD", {required: true})} />
+            <div style={{paddingLeft: "0.5%"}}>
+                <ul style={{width: "70%"}}
+                    className="menu menu-vertical lg:menu-horizontal">
+                    <div style={{width: "27%", paddingRight: "2%"}}>
+                        <ul style={{width: "100%", paddingTop: "2%", paddingBottom: "2%"}}
+                            className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+                            <div className="font-bold" style={{paddingRight: "1%"}}>Enrollment deadline:</div>
+                            <input type="date" {...register("ETD", {required: true})} />
+                        </ul>
+                    </div>
+
+                    <div style={{width: "21%"}}>
+                        <ul style={{width: "100%", paddingTop: "2%", paddingBottom: "2%"}}
+                            className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+                            <div className="font-bold" style={{paddingRight: "1%"}}>Final deadline:</div>
+                            <input type="date" {...register("FTD", {required: true})}/>
+                        </ul>
+                    </div>
+
+
+                </ul>
             </div>
             <div style={{padding: "1%"}}>
-                <input  className="textarea textarea-primary"{...register("maxNumberOfStudents", {required: true})} placeholder="Max students allowed..."/>
+                <input className="textarea textarea-primary"{...register("maxNumberOfStudents", {required: true})}
+                       placeholder="Max students allowed..."/>
             </div>
             <div style={{padding: "1%"}}>
                 <select {...register("tournamentPolicy")}>
@@ -52,31 +71,34 @@ export default function CreateTournament() {
 
             </div>
             <div style={{padding: "1%"}}>
-                <input type="submit"/>
+                <button className="btn btn-primary">
+                    <input type="submit" value="Submit"/>
+                </button>
+
             </div>
         </form>
-)
+    )
 
-/*return (
-        <>
-            <h1 className="text-3xl font-bold" style={{padding:"1%"}}>Create Tournament</h1>
+    /*return (
+            <>
+                <h1 className="text-3xl font-bold" style={{padding:"1%"}}>Create Tournament</h1>
 
-            <form style={{padding: "1%"}}>
-                <input type="textarea" style={{width: "33.33%"}} className="textarea textarea-primary"
-                       name="TournamentTitle" id="TournamentTitle"
-                       placeholder="Tournament Title..." required/>
-                <div style={{paddingTop: "0.5%"}}>
-                    <textarea style={{width: "99%"}} className="textarea textarea-primary textarea-lg"
-                              name="Description" id="Description"
-                              placeholder="Description..." required/>
-                </div>
-                <ul className="menu menu-vertical lg:menu-horizontal">
+                <form style={{padding: "1%"}}>
+                    <input type="textarea" style={{width: "33.33%"}} className="textarea textarea-primary"
+                           name="TournamentTitle" id="TournamentTitle"
+                           placeholder="Tournament Title..." required/>
+                    <div style={{paddingTop: "0.5%"}}>
+                        <textarea style={{width: "99%"}} className="textarea textarea-primary textarea-lg"
+                                  name="Description" id="Description"
+                                  placeholder="Description..." required/>
+                    </div>
+                    <ul className="menu menu-vertical lg:menu-horizontal">
 
-                    <li><a> <input  type="date" name="EnrollDeadline" id="EnrollDeadline" placeholder="EnrollDeadline" required/></a></li>
-                    <li><a><input  type="date" name="FinalDeadline" id="FinalDeadline" placeholder="FinalDeadline" required /></a></li>
-                </ul>
-            </form>
-        </>
+                        <li><a> <input  type="date" name="EnrollDeadline" id="EnrollDeadline" placeholder="EnrollDeadline" required/></a></li>
+                        <li><a><input  type="date" name="FinalDeadline" id="FinalDeadline" placeholder="FinalDeadline" required /></a></li>
+                    </ul>
+                </form>
+            </>
 
-    )*/
+        )*/
 }
