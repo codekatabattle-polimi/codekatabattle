1e1751bbed48ad1043ba39900974434e94f8193a
Peppisparrow <127871840+Peppisparrow@users.noreply.github.com>
2024-02-02 20:17:05 +0100

    update CreateTournament.tsx addCoordinator

--- IT/codekatabattle-frontend/src/components/VisualizeTournaments.tsx
+++ IT/codekatabattle-frontend/src/components/VisualizeTournaments.tsx
@@ -1,81 +1,85 @@
-import {PageTournament, Tournament, TournamentService} from "../services/openapi";
+import { PageTournament, Tournament, TournamentService} from "../services/openapi";
 import {useEffect, useState} from "react";
 import {NavBar} from "./NavBar.tsx";
 import earth from "../assets/earth.png"
 import {useNavigate, useParams} from "react-router-dom";
 import student from "../assets/reading.png";
 import educator from "../assets/educator.png";
-
+import avatar from "../assets/avatar2.png"
 export const VisualizeTournaments= () => {
     const {page} = useParams();
     const [tournaments, setTournaments] = useState<PageTournament | null>(null);
     const [error, setError] = useState<Error | null>(null);
-    const navigate=useNavigate();
-
+    const navigate = useNavigate();
     useEffect(() => {
         fetchTournaments();
     }, [page]);
 
-    async function fetchTournaments(){
+    async function fetchTournaments() {
         try {
-            const tournaments = await TournamentService.findAll(+page!,5);
+            const tournaments = await TournamentService.findAll(+page!, 5);
             setTournaments(tournaments);
-        } catch (error) {
-            setError(error as Error);
+
+        } catch (error1) {
+            setError(error1 as Error);
+            alert((error?.message ?? "Error"));
         }
     }
-    function to(id:string|undefined){
-        if (id!=undefined) {
-            const s="/tournaments/"+id;
+
+    function to(id: string | undefined) {
+        if (id != undefined) {
+            const s = "/tournaments/" + id;
             navigate(s);
         }
     }
-    function buttomPage(){
-        if(page==undefined)return (<></>)
-        if(tournaments==undefined)return (<></>)
-        if(tournaments.totalPages==undefined)return (<></>)
-        if(tournaments.totalPages<=1)return (
+
+    function buttomPage() {
+        if (page == undefined) return (<></>)
+        if (tournaments == undefined) return (<></>)
+        if (tournaments.totalPages == undefined) return (<></>)
+        if (tournaments.totalPages <= 1) return (
             <div className="join">
 
             </div>
         )
-        if(tournaments.totalPages<=2 && page=="0"){
-            return(
+        if (tournaments.totalPages <= 2 && page == "0") {
+            return (
                 <div className="join">
                     <input className="join-item btn " type="radio" name="options" aria-label="1"
                            checked/>
                     <input className="join-item btn " type="radio" name="options" aria-label="2"
-                           onClick={()=>(to("view/1"))}/>
+                           onClick={() => (to("view/1"))}/>
                 </div>
             )
         }
-        if(tournaments.totalPages<=2 && page=="1"){
-            return(
+        if (tournaments.totalPages <= 2 && page == "1") {
+            return (
                 <div className="join">
                     <input className="join-item btn " type="radio" name="options" aria-label="1"
-                           onClick={()=>(to("view/0"))}/>
+                           onClick={() => (to("view/0"))}/>
                     <input className="join-item btn " type="radio" name="options" aria-label="2"
                            checked/>
                 </div>
             )
         }
-        if(page=="0"){
-            return(
+        if (page == "0") {
+            return (
                 <div className="join">
                     <input className="join-item btn " type="radio" name="options"
                            aria-label={"Page " + (+page! + 1).toString()}
                            checked/>
                     <input className="join-item btn " type="radio" name="options" aria-label="»"
-                           onClick={() => (to("view/"+(+page!+1).toString()))}/>
+                           onClick={() => (to("view/" + (+page! + 1).toString()))}/>
                     <input className="join-item btn " type="radio" name="options"
                            aria-label={(tournaments.totalPages - 1).toString()}
-                           onClick={() => (to("view/"+((tournaments?.totalPages ?? 2)-2).toString()))} />
-                    <input className="join-item btn " type="radio" name="options" aria-label={tournaments.totalPages.toString()}
-                           onClick={() => (to("view/"+((tournaments?.totalPages ?? 1)-1).toString()))} />
+                           onClick={() => (to("view/" + ((tournaments?.totalPages ?? 2) - 2).toString()))}/>
+                    <input className="join-item btn " type="radio" name="options"
+                           aria-label={tournaments.totalPages.toString()}
+                           onClick={() => (to("view/" + ((tournaments?.totalPages ?? 1) - 1).toString()))}/>
                 </div>
             )
         }
-        if (page == (tournaments.totalPages-1).toString()) {
+        if (page == (tournaments.totalPages - 1).toString()) {
             return (
                 <div className="join">
                     <input className="join-item btn " type="radio" name="options" aria-label="1"
@@ -84,7 +88,7 @@ export const VisualizeTournaments= () => {
                     <input className="join-item btn " type="radio" name="options" aria-label="2"
                            onClick={() => (to("view/1"))}/>
                     <input className="join-item btn " type="radio" name="options" aria-label="«"
-                           onClick={() => (to("view/"+(+page!-1).toString()))}
+                           onClick={() => (to("view/" + (+page! - 1).toString()))}
                     />
                     <input className="join-item btn " type="radio" name="options"
                            aria-label={"Page " + (+page! + 1).toString()}
@@ -114,85 +118,109 @@ export const VisualizeTournaments= () => {
         </div>)
     }
 
+
     function tournamentRows() {
         if (tournaments == null) return (<></>);
         if (tournaments.content == null) return (<></>);
-        return (tournaments.content.map((t: Tournament) => (
-
-                <div className="stats shadow " style={{width: "100%"}} onClick={()=>to(t.id?.toString())}>
-
-                        <div className="stat " style={{overflow: "auto"}}>
+        return(
+            tournaments.content.map((t: Tournament) => (
+                <tr className=" shadow " style={{width: "100%"}} onClick={() => to(t.id?.toString())}>
+                    <th style={{width: "10%", overflow: "auto"}}>
+                        <div className=" ">
                             <div className="stat-title text-xs">Title</div>
                             <div className="stat-value text-2xl">{t.title}
                             </div>
                         </div>
-
-
-                    <div className="stat" style={{overflow: "auto"}}>
-                        <div className="stat-title text-xs">Total Coordinators</div>
-                        <div className="navbar " style={{}}>
-                            <div className=" stat-value text-secondary text-2xl"
-                                 style={{width: "20%"}}>{t.coordinators?.length ?? 0}</div>
-                            <img className="" src={educator} style={{position: "relative", width: "15%", left: "65%"}}/>
-                        </div>
-                        <div className="stat-desc"></div>
-                    </div>
-                    <div className="stat" style={{overflow: "auto"}}>
-                        <div className="stat-title text-xs">Total Participants</div>
-                        <div className="navbar " style={{}}>
-                            <div className=" stat-value text-primary text-2xl"
-                                 style={{width: "20%"}}>{(t.participants?.length ?? 0)}</div>
-                            <img className="" src={student} style={{position: "relative", width: "15%", left: "65%"}}/>
-                        </div>
-                        <div className="stat-desc"></div>
-                    </div>
-
-                    <div className="stat" style={{overflow: "auto"}}>
-                        <div className="stat-figure"style={{paddingTop:"25%"}}>
-                            <div
-                                className="stat-desc">{(t.maxParticipants ?? 1000) - (t.participants?.length ?? 0)} available
-                                spot
+                    </th>
+                    <th>
+                        <div className="" style={{overflow: "auto"}}>
+                            <div className="stat-title text-xs">Total Coordinators</div>
+                            <div className="navbar " style={{}}>
+                                <div className=" stat-value text-secondary text-2xl"
+                                     style={{width: "20%"}}>{t.coordinators?.length ?? 0}</div>
+                                <img className="" src={educator}
+                                     style={{position: "relative", width: "15%", left: "65%"}}/>
                             </div>
+                            <div className="stat-desc"></div>
                         </div>
-                        <div className="stat-title text-xs" style={{paddingBottom: "4%"}}>Occupation</div>
-                        <div className="stat-value text-secondary">
-                            <div className="radial-progress text-secondary"
-                                 style={{"--value": (t.participants?.length ?? 0) / (t.maxParticipants ?? 1000), "--size":"3rem"} as React.CSSProperties}
-                                 role="progressbar"><p
-                                className="text-xs">{Math.min(Math.ceil((t.participants?.length ?? 0) / (t.maxParticipants ?? 1000)),100)}</p>
+                    </th>
+                    <th>
+                        <div className="" style={{overflow: "auto"}}>
+                            <div className="stat-title text-xs">Total Participants</div>
+                            <div className="navbar " style={{}}>
+                                <div className=" stat-value text-primary text-2xl"
+                                     style={{width: "20%"}}>{(t.participants?.length ?? 0)}</div>
+                                <img className="" src={student}
+                                     style={{position: "relative", width: "15%", left: "65%"}}/>
                             </div>
+                            <div className="stat-desc"></div>
                         </div>
-                    </div>
+                    </th>
 
-                    <div className="stat" style={{overflow: "auto"}}>
-                        <div className="stat-figure text-secondary">
-                            <div className="avatar online">
-                                <div className="w-16 rounded-full">
-                                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                    <th>
+                        <div className="stat" style={{overflow: "auto"}}>
+                            <div className="stat-figure" style={{paddingTop: "25%"}}>
+                                <div
+                                    className="stat-desc">{(t.maxParticipants ?? 1000) - (t.participants?.length ?? 0)} available
+                                    spot
+                                </div>
+                            </div>
+                            <div className="stat-title text-xs" style={{paddingBottom: "4%"}}>Occupation</div>
+                            <div className="stat-value text-secondary">
+                                <div className="radial-progress text-secondary"
+                                     style={{
+                                         "--value": (t.participants?.length ?? 0) / (t.maxParticipants ?? 1000),
+                                         "--size": "3rem"
+                                     } as React.CSSProperties}
+                                     role="progressbar"><p
+                                    className="text-xs">{Math.min(Math.ceil((t.participants?.length ?? 0) / (t.maxParticipants ?? 1000)), 100)}</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="stat-title" style={{paddingTop:"10%"}}>created by</div>
-                        <div className="stat-desc text-primary">{t.creator}</div>
-                    </div>
+                    </th>
 
-                </div>
+                    <th>
+                        <div className="stat" style={{overflow: "auto"}}>
+                            <div className="stat-figure text-secondary">
+                                <div className="avatar online">
+                                    <div className="w-16 rounded-full">
+                                        <img src={avatar}/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="stat-title" style={{paddingTop: "10%"}}>created by</div>
+                            <div className="stat-desc text-primary">{t.creator}</div>
+                        </div>
+                    </th>
 
-        )))
+
+                </tr>
+            ))
+        )
+
     }
 
-    if (error) return (<>{error.message}</>);
-    if (tournaments == null) return (<>Niente tornei</>);
+    if (tournaments == null) return (<>no tournament</>);
     return (
         <>
-            <div style={{alignSelf: "end", top: "8%", position: "fixed", width: "100%"}}>
+            <div style={{alignSelf: "end", top: "8%",position:"fixed", width: "100%"}}>
 
                 <ul className="menu menu-vertical lg:menu-horizontal " style={{width: "100%"}}>
                     <img src={earth} style={{width: "3%", height: "3%", paddingLeft: "1%", paddingTop: "0.1%"}}/>
                     <h1 className="text-3xl font-bold"
                         style={{paddingLeft: "0.5%", paddingBottom: "0.5%"}}>Tournament</h1>
                 </ul>
-                {tournamentRows()}
+                <div className="overflow-x-auto">
+                    <table className="table " style={{width: "100%"}}>
+                        <thead style={{width: "100%"}}>
+
+                        </thead>
+                        <tbody>
+                        {tournamentRows()}
+                        </tbody>
+                    </table>
+                </div>
+
                 <div className="flex flex-col w-full">
                     <div className="divider">
                         {buttomPage()}
